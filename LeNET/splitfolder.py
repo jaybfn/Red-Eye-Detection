c729de9e76f969553ec3a168666f46f0de2c405b
jaybfn <jayesh.bfn@gmail.com>
2022-08-21 15:20:06 +0200

    final

--- LeNET/splitfolder.py
+++
@@ -1,8 +0,0 @@
-# import libraries
-import splitfolders as sf
-
-# path
-FOLDER_NAME = '../data/eye_data/'
-
-# splitfolder
-sf.ratio(FOLDER_NAME, output="../data/eye_data/training", seed=142, ratio=(0.8,0.15,0.05))
